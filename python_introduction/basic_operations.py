c44d38c38d8823543daefe98aab66012a2d8a54e
Mahider Tsegaye <Mahidernardos@gmail.com>
2025-07-07 12:12:46 +0300

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+Subtruction=number1-number2
+Addition=number1+number2
+Multiplication=number1*number2
+print(f"Addition of {number1} and {number2} is {Addition}")
+print(f"Subtraction of {number1} and {number2} is {Subtruction}")
+print(f"Multiplication of {number1} and {number2} is {Multiplication}")
