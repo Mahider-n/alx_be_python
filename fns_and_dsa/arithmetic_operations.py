70fa53b9704ba3698e744d2a0dcdfa91de90b82d
Mahider Tsegaye <Mahidernardos@gmail.com>
2025-07-25 08:28:27 +0300

    arthmetic operation implentation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,17 +2,6 @@
 # arithmetic_operations.py
 
 def perform_operation(num1: float, num2: float, operation: str):
-    """
-    Performs basic arithmetic operations.
-
-    Parameters:
-        num1 (float): The first number.
-        num2 (float): The second number.
-        operation (str): The operation to perform - 'add', 'subtract', 'multiply', 'divide'.
-
-    Returns:
-        float or str: The result of the arithmetic operation or an error message for invalid operations.
-    """
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
