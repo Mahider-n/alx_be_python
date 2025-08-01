eb0540c5db8248e03992450cd9f4a8908ee2921e
Mahider Tsegaye <Mahidernardos@gmail.com>
2025-08-01 14:44:45 +0300

    library_management implementaion

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,52 @@
+# library_management.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title               # public
+        self.author = author             # public
+        self.__is_checked_out = False    # private
+
+    def check_out(self):
+        if not self.__is_checked_out:
+            self.__is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self.__is_checked_out:
+            self.__is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self.__is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self.__books = []  # private list of Book objects
+
+    def add_book(self, book):
+        self.__books.append(book)
+
+    def check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+        print(f"'{title}' is not available for checkout.")
+
+    def return_book(self, title):
+        for book in self.__books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+        print(f"'{title}' was not checked out or does not exist.")
+
+    def list_available_books(self):
+        available_books = [book for book in self.__books if book.is_available()]
+        if not available_books:
+            print("No books are currently available.")
+        else:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
